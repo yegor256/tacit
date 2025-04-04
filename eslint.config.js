4e9d7ff5155b03fc27c6beca92ec28f94b33e8db
Yegor Bugayenko <yegor256@gmail.com>
2025-04-05 01:18:35 +0800

    #305: typo

--- eslint.config.js
+++ eslint.config.js
@@ -17,10 +17,10 @@ module.exports = [
     rules: {
       ...configs.all.rules,
       'indent': ['error', 2],
-      'max-len': ['error', { code: 200 }]
+      'max-len': ['error', { code: 200 }],
       'max-lines-per-function': 'off',
       'no-magic-numbers': 'off,'
-      'no-undef': 'off',
+      'no-undef': 'off'
     }
   }
 ];
